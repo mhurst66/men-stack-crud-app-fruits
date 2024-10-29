1e3618bf33bed4895c2da95c2e9710381b1d1c5c
mhurst <matthewhurst1791@gmail.com>
2024-10-28 21:00:55 -0400

    fruits app

---
+++ models/fruit.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+
+
+
+// first define the shape of the object we want to store in our database
+// this is a SCHEMA
+
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    isReadyToEat: Boolean,
+})
+
+// Tell mongoose that the model we want to generate is based off the schema and provide a name to it
+// mongoose.model("Name", schema)
+
+const Fruit = mongoose.model("Fruit", fruitSchema)
+
+
+// share it with the rest of your application
+// module.exports = modelName
+
+module.exports = Fruit
+
+
+
+// when we talk about the shape of an object we are talking about the fields and inputs of that object
+// this allows us to hold all the data without tons of data structures all over the place
+// 
+// This is where models make sense each singular model is a different data structure and we hold each model in the folder models!
--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-// Here is where we import modules
-// We begin by loading Express
-const express = require("express");
 
-const app = express();
+require('dotenv').config() // Requires & Loads the environment file and variables from .env file this should be the first line in the server so the rest of the application can use the "secret codes"
+require('./config/database') // This requires the server to open the database from our config file
+const express = require("express")
 
-
-
-
+// Models
+const Fruit = require('./models/fruit')
 
 
+const app = express()
+const PORT = process.env.PORT ? process.env.PORT : '3000'
 
 
-// server.js
-
-// GET /
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
-  });  
+    res.render("index.ejs")
+})
 
 
 
@@ -27,5 +24,5 @@ app.get("/", async (req, res) => {
 
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+  console.log("Listening on port 3000")
+})
