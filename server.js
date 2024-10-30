17aeb2d3949b0a13dcec0d8d6263cced526ced35
mhurst <matthewhurst1791@gmail.com>
2024-10-29 20:14:25 -0400

    Create a fruit complete

--- server.js
+++ server.js
@@ -4,23 +4,37 @@ require('dotenv').config() // Requires & Loads the environment file and variable
 
 require('./config/database') // This requires the server to open the mongoose database from our config file
 
-const express = require("express")
+const express = require("express") // requires express
+const app = express() // creates the express app and allows us to use .use, etc.
+const PORT = process.env.PORT ? process.env.PORT : '3000'
 
 // Models
 const Fruit = require('./models/fruit')
-
-
-const app = express()
-const PORT = process.env.PORT ? process.env.PORT : '3000'
+// handles data from form
+app.use(express.urlencoded({ extended: false }))
 
 
 // GET ROUTES
+// index.ejs
 app.get("/", async (req, res) => {
     res.render("index.ejs")
 })
-
+// fruits/new.ejs 
 app.get("/fruits/new", (req, res) => {
-  res.send("This route sends the user a form page!")
+  res.render("fruits/new.ejs")
+})
+
+// POST ROUTES
+// new fruit
+app.post("/fruits", async (req, res) => {
+  console.log(req.body)
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true
+  } else {
+    req.body.isReadyToEat = false
+  }
+  await Fruit.create(req.body)
+  res.redirect("/fruits/new")
 })
 
 
@@ -33,6 +47,7 @@ app.get("/fruits/new", (req, res) => {
 
 
 
+// LISTEN FOR THE SERVER
 app.listen(3000, () => {
   console.log("Listening on port 3000")
 })
